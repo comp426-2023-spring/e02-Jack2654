79252823cf9ee33130e53c77fac76994fd1bf1ba
Jack2654 <jack.morgenstein@gmail.com>
2023-05-07 12:43:57 -0400

    tenth commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express');
 const __dirname = path.resolve();
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
