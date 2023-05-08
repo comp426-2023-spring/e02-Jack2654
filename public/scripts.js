80018f8d6d8e7ce401d50eaddc9146d8b1a86195
Jack2654 <jack.morgenstein@gmail.com>
2023-05-08 14:44:40 -0400

    19: more attempted fixes

--- public/scripts.js
+++ public/scripts.js
@@ -1,18 +1,110 @@
-async function getUsers() {
-    alert("getting result")
-    var endpoint = '/app/rps_random_result';
-    try {
-        let res = await fetch(endpoint);
-        return await res.json();
-    } catch (error) {
-        console.log(error);
-    }
-}
-async function renderUsers() {
-    let users = await getUsers();
-    let html = `<div>${users}</div>`;
-    let container = document.querySelector('.container');
-    container.innerHTML = html;
+// If you would like to see some examples of similar code to make an interface interact with an API, 
+// check out the coin-server example from a previous COMP 426 semester.
+// https://github.com/jdmar3/coinserver
+
+var rps = true;
+var opponent = false; 
+const moves = ["rock", "paper","scissors","lizard", "spock"];
+var move = "rock";
+
+function hidethis(){
+    document.getElementById("rpsls_move_options").hidden = true; 
 }
 
-renderUsers();
+function rules(){
+    var x = document.getElementById("view_rules");
+    if(x.style.display === "none"){
+        x.style.display="flex";
+    } else {
+        x.style.display="none";
+    }
+}
+
+function reset(){
+    rps=true;
+    opponent=false;
+    move="rock";
+    document.getElementById("game_options").hidden = false;
+    document.getElementById("rpsls_move_options").hidden = true; 
+    document.getElementById("results").hidden = true; 
+    document.getElementById("rps").checked = false;
+    document.getElementById("rpsls").checked = false;
+    document.getElementById("opponent").checked = false;
+    
+}
+
+function rpsgame(){
+    rps = true;
+}
+
+function rpslsgame(){
+    rps = false;
+}
+
+function opponentgame(){
+    opponent = true;
+}
+
+function playgame(){
+    
+    if (!document.getElementById("rps").checked && !document.getElementById("rpsls").checked)
+        window.alert("Must select a game to play");
+    else{
+        if(!opponent){
+            makeMove()
+        }
+        else{
+            document.getElementById("game_options").hidden = true;
+            document.getElementById("rpsls_move_options").hidden = false; 
+            if (rps){
+                var dropdown = document.getElementById("your_move");
+                dropdown.remove(4);
+                dropdown.remove(3);
+            }
+        } 
+    }
+
+}
+
+function makeMove(){
+    if(!opponent) {
+        if(rps){
+            var randMove = moves[Math.floor(Math.random()* 3)];
+            var api = "/app/rps/play/" + randMove;
+            fetch(api).then(response => response.json()).then(data => {
+                document.getElementById("results").innerText = JSON.stringify(data);
+                document.getElementById("game_options").hidden = true; 
+                document.getElementById("results").hidden = false; 
+            });
+        } else {
+            var randMove = moves[Math.floor(Math.random()* 5)];
+            var api = "/app/rpsls/play/"+randMove;
+            fetch(api).then(response => response.json()).then(data => {
+                document.getElementById("results").innerText = JSON.stringify(data);
+                document.getElementById("game_options").hidden = true; 
+                document.getElementById("results").hidden = false; 
+            });
+
+        }
+    }
+    else{
+        var index = document.getElementById("your_move");
+        move = moves[index.value];
+        if(rps){
+            var api = "/app/rps/play/" + move;
+            fetch(api).then(response => response.json()).then(data => {
+                document.getElementById("results").innerText = JSON.stringify(data);
+                document.getElementById("rpsls_move_options").hidden = true; 
+                document.getElementById("results").hidden = false; 
+            });
+        } else {
+            var api = "/app/rpsls/play/"+ move;
+            fetch(api).then(response => response.json()).then(data => {
+                document.getElementById("results").innerText = JSON.stringify(data);
+                document.getElementById("rpsls_move_options").hidden = true; 
+                document.getElementById("results").hidden = false; 
+            });
+        }
+    } 
+
+}
